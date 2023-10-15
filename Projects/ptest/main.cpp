5bc490f1155a3de4d947c48e1f506d2b56c56a53
SFS-RAID <99122481+SFS-RAID@users.noreply.github.com>
2023-10-15 04:23:24 +0000

    First commit

---
+++ Projects/ptest/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main(){
+    cout << "hello\n";
+    vector<int> pointlist;
+    vector<string> pointype;
+
+    string abcd = "nl", input;
+    cout << "Press enter key to start";
+    cin >> abcd;
+    
+    for (int i = 1; i < 5; i++)
+    {
+        /* code */
+    }
+    
+    
+}
---
+++ Projects/ptest/questions.hpp
@@ -0,0 +1,10 @@
+#ifndef QUESTIONS_H
+#define QUESTIONS_H
+
+int introvert, extrovert;
+int intuition, sensing;
+int thinking, feeling;
+int perceiving, judging;
+int assertive, turbulent;
+
+#endif
